c9a272a0f6369620bfbd2dfd4b4a486c48df25fc
Anthony Cagliano <acagliano97@gmail.com>
2023-07-26 18:49:55 -0400

    begin redesign

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TInyAuth | TI-84+ CE Credentials Grant and Authentication</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <style>
+            html, body {width:100vw; height:100vh; margin:0; padding:0; outline:0;}
+            html {background:white;}
+            body {
+                background: rgb(86,60,92);
+                background: hsla(71, 83%, 33%, 1);
+                background: radial-gradient(circle, rgba(80,64,77, .75) 0%, rgba(80,64,77, 1) 100%);
+                background: -moz-radial-gradient(circle, rgba(80,64,77, .75) 0%, rgba(80,64,77, 1) 100%);
+                background: -webkit-radial-gradient(circle, rgba(80,64,77, .75) 0%, rgba(80,64,77, 1) 100%);
+            }
+            #header {
+                background:rgba(254,254,254,.95);
+                width:100%; height:12%;
+                color:black;
+                font-size:150%;
+                box-shadow:2px 2px 10px black;
+                display:flex;
+                flex-direction:row;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="header">
+            <img id="ta-img" src="logo.png" />
+            TInyAuth</div>
+        <div id="content">
+        </div>
+    </body>
+</html>
