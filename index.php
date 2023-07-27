7812f179f24de1190da6eb103ff393e701b4925f
Anthony Cagliano <acagliano97@gmail.com>
2023-07-27 00:14:42 -0400

    Header complete except for image

--- index.php
+++ index.php
@@ -5,30 +5,84 @@
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <style>
+            @font-face {
+                font-family: "Share Tech";
+                src:
+                    url("fonts/sharetech/ShareTechMono-Regular.ttf") format("truetype")
+            }
+
             html, body {width:100vw; height:100vh; margin:0; padding:0; outline:0;}
             html {background:white;}
             body {
                 background: rgb(86,60,92);
-                background: hsla(71, 83%, 33%, 1);
-                background: radial-gradient(circle, rgba(80,64,77, .75) 0%, rgba(80,64,77, 1) 100%);
-                background: -moz-radial-gradient(circle, rgba(80,64,77, .75) 0%, rgba(80,64,77, 1) 100%);
-                background: -webkit-radial-gradient(circle, rgba(80,64,77, .75) 0%, rgba(80,64,77, 1) 100%);
+                background: radial-gradient(circle, rgba(80,64,77, .80) 0%, rgba(80,64,77, 1) 100%);
+                background: -moz-radial-gradient(circle, rgba(80,64,77, .80) 0%, rgba(80,64,77, 1) 100%);
+                background: -webkit-radial-gradient(circle, rgba(80,64,77, .80) 0%, rgba(80,64,77, 1) 100%);
             }
             #header {
-                background:rgba(254,254,254,.95);
-                width:100%; height:12%;
+                position:relative;
+                background:rgba(255,255,255,1);
+                width:100%; height:13%;
                 color:black;
-                font-size:150%;
                 box-shadow:2px 2px 10px black;
                 display:flex;
                 flex-direction:row;
+                justify-content:space-between;
             }
+            #header>* {display:flex; align-items:center; justify-content:center;}
+            #ta-icon {width:auto; flex:0;}
+            #title {
+                width:auto;
+                font-size:64px;
+                font-family:"Share Tech";
+                font-weight:900;
+                flex:0; height:100%;
+                text-stroke:2px black;
+                -webkit-text-stroke:2px black;
+            }
+            #infoline-nav {flex:0 1 50%; display:flex; flex-direction:column; height:100%; width:60%;}
+            #infoline-nav>* {display:flex; align-items:center; justify-content:center;}
+            #infoline {color:blue; width:100%; height:50%; font-size:20px; font-family:"Share Tech";}
+            #navigation {
+                width:100%;
+                height:40%;
+                display:flex;
+            }
+            .navitem {margin:0 2%; flex:1; justify-content:center; height:100%; display:flex; align-items:center; font-family:"Share Tech";}
+           // .navitem:hover {background:rgba(80,64,77,.25); cursor:pointer; cursor:hand;}
+           .navitem:not(#login-button):hover {background: linear-gradient(to top, rgba(0,0,255,.5) 0%, rgba(255,255,255,1) 100%);}
+
+            #login-button {
+                height:80%;
+                position:relative;
+                display:flex; flex-direction:row;
+                justify-content: space-between;
+                background:blue;
+                border:3px solid silver;
+                color:white;
+                font-weight:bold;
+                border-radius:10px;
+            }
+            #login-button:hover {}
+            #login-button>#login-button-padlock {position:absolute; left:-20px; flex:0; font-size:26px; border-radius:50%; padding:5px 10px; background:blue; border:3px solid silver;}
+            #login-button>#login-button-text {width:100%; text-align:center; font-family:"Share Tech";}
         </style>
     </head>
     <body>
         <div id="header">
-            <img id="ta-img" src="logo.png" />
-            TInyAuth</div>
+            <div id="ta-icon"><img id="ta-img" src="ta-logo.png" /></div>
+            <div id="title">TInyAuth</div>
+            <div id="infoline-nav">
+                <div id="infoline">• TI-84+ CE AuthKey Issuing &amp; Authentication Service •</div>
+                <div id="navigation">
+                    <div class="navitem">Terms of Service</div>
+                    <div class="navitem">Privacy Policy</div>
+                    <div id="login-button" class="navitem">
+                        <div id="login-button-padlock">&#x1f512;</div>
+                        <div id="login-button-text">My Account</div>
+                    </div>
+                </div>
+            </div>
         <div id="content">
         </div>
     </body>
