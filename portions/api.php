4e5af4d9a55a3009f3fcfe7c85075e40aa12dbe6
Anthony Cagliano <acagliano97@gmail.com>
2023-08-03 14:01:26 -0400

    add API documentation

--- portions/api.php
+++ portions/api.php
@@ -36,7 +36,7 @@
                     <span class="heading">Extracting Credentials</span><br />
                     Once selected, the file can be opened using the <span style="font-family:monospace;">ti_Open</span> function from the <span style="font-family:monospace;">fileioc</span> toolchain standard library or using the <span style="font-family:monospace;">fopen</span> API from the C standard also implemented within the toolchain. The file can be read in-place but it is recommended to make a copy and perform decoding on that both to ensure scope and to allow for decryption without altering the file.<br /><br />
                     The ASN.1-encoded structure begins at the end of the 6-byte prefix string meaning you will need to begin decoding at that point. The ASN.1 structure of the keyfile is as follows:<br />
-                    <pre style="background:rgba(0,0,0,.2); padding:2%; width:90%;">
+                    <pre style="font-family:monospace; background:rgba(0,0,0,.2); padding:2%; width:90%;">
 KeyNormal     :: SEQUENCE {
     Encrypted   BOOLEAN,
     Credentials :: SEQUENCE {
@@ -60,16 +60,69 @@ where: Credentials, Tag = Cipher(AES-256-GCM,
         // and the last 16 are the iv/nonce
         // the user should supply the password as input during key load, or the password
         // can be read from some arbitrary on-device password manager</pre>
-                </li>
+                </li><br />
                  <li>
                     <span class="heading">Serializing for Transfer</span><br />
                     Once the keyfile data has been processed, the client then needs to transfer it to the server in an understandable format. The recommended means of doing so involve encoding the credentials as zero-terminated strings or as length-prepended strings. You are not restricted to this, just make sure that whatever serialization method you use, you de-serialize using an appropriate algorithm on the server.
-                </li>
+                </li><br />
                 <li>
                     <span class="heading">The TInyAuth Static Library</span><br />
-                    We have made available a static library to handle all of the aforementioned keyfile decoding and serialization. This leaves the application developer to handle only keyfile selection and actual transmission of data. The library is available at the TInyAuth github <a href="">
+                    We have made available a static library to handle all of the aforementioned keyfile decoding and serialization. This leaves the application developer to handle only keyfile selection and actual transmission of data. The library is available at the <a href="https://github.com/acagliano/tinyauth/tree/ccbe57e3f7f2c0c0cde1baea2d3f2b1c51b617c3/client-library" target="_blank">TInyAuth Github</a>. NOTE: Requires <a href="https://github.com/acagliano/cryptx">Cryptx</a> library. Use of the library API is quite simple:
+                    <pre style="font-family:monospace; background:rgba(0,0,0,.2); padding:2%; width:90%;">
+char *var_name;
+void *vat_ptr = NULL;
+char keyfiles[128][9];   // support up to 128 keyfiles
+size_t keyfile_count = 0;
+
+while ((var_name = ti_Detect(&vat_ptr, "TIAUTH"))){
+    strcpy(keyfiles[keyfile_count], var_name);
+    keyfile_count++;
+}
+char* key_selected = user_select_key(keyfiles, keyfile_count);
+// ^ this would be an API that lets the user select the key
+char* password = prompt_for_user_password();
+// ^ this would be an API that prompts for password
+// Allow user to return empty string if no password needed
+
+struct tinyauth_key k;
+tinyauth_open(&k, key_selected, password);
+uint8_t buf[k.credentials_len]; // this field can approximate needed output size
+size_t olen = tinyauth_serialize_for_transfer(&k, buf, TA_SERIALIZE_0TERM);
+tinyauth_close(&k);
+// ^ This frees allocated memory and destroys the copy of the key
+// Do not forget this or you may leave decrypted keyfile in memory.
+
+network_send(buf, olen);
+memset(buf, 0, olen);   // wipe this out once done as well</pre>
                 </li>
-            </ol>
+            </ol><br />
+            <h2>Server-Side</h2>
+            <p>Use of TInyAuth on the server-side is even simpler. Once you deserialize the credentials you can send a POST request to TInyAuth with the credentials supplied as parameters. Here is some code demonstrating how to do this, in Python.</p>
+            <pre style="font-family:monospace; background:rgba(0,0,0,.2); padding:2%; width:90%;">
+import requests
+
+// assume received data is in 'data' and serialization method is TA_SERIALIZE_OTERM
+
+# deserialize credentials packet
+creds = data.split("\0", maxsplit=1)
+# ^ maxsplit prevents token from mistakenly being split if a byte happens to be 0
+
+# send POST request
+uri = "https://tinyauth.cagstech.com/auth.php"
+response = requests.post(
+    uri,
+    params={'user': creds[0], 'token': creds[1]},
+)
+
+# check response
+if response.json["success"] == True:
+    // user authenticated successfully
+else:
+    // user did not authenticate
+    // error string in response.json["error"]
+    // if error string unset, credentials were incorrect
+    // else, internal error most likely</pre>
+    <p>It&apos;s really that simple.</p>
         </div>
     </body>
 </html>
