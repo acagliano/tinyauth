50da618f4b09f7cd0268bc75aba00c6d086a8b8f
Anthony Cagliano <acagliano97@gmail.com>
2023-10-03 16:04:17 -0400

    fix vendor autoload ref

--- scripts/send-email.php
+++ scripts/send-email.php
@@ -3,7 +3,7 @@ $env = parse_ini_file(".env");
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
-require "..vendor/autoload.php";
+require "../vendor/autoload.php";
 
 function send_email($to, $subject, $body, $isHTML=true){
 
