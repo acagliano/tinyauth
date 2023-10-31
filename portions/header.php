7e346278934193e427530cc7816d9d3cfb5481e6
Anthony Cagliano <acagliano97@gmail.com>
2023-10-31 00:35:07 -0400

    add error output

--- portions/header.php
+++ portions/header.php
@@ -3,9 +3,16 @@
         <div id="title">TInyAuth</div>
         <div id="infoline">Key-Based Authentication<br />for the TI-84+ CE</div>
         <form id="login" action="<?php echo filter_input(INPUT_SERVER, "PHP_SELF", FILTER_SANITIZE_URL); ?>" method="post">
+                <p>Login to access Account Dashboard</p>
                 <input type="email" name="email" placeholder="Email" required />
                 <input type="password" name="password" placeholder="Password" required />
                 <button class="g-recaptcha" data-sitekey="<?php echo $env['RECAPTCHA_SITEKEY'];?>" data-callback='onLogin'  data-action='submit'>Login</button>
+
+                <?php
+                foreach($l_errors as $l){
+                        echo $l."<br />";
+                }
+                ?>
         </form>
         <div id="navigation">
                 <div class="navitem"><a href="/portions/api.php">Documentation</a></div>
