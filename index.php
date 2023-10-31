7b2d0bc72c52da89cc63d77109836d7dd5ec021c
Anthony Cagliano <acagliano97@gmail.com>
2023-10-31 00:59:59 -0400

    try it this way

--- index.php
+++ index.php
@@ -95,6 +95,10 @@ if(isset($_POST["login"])){
         <script src="/scripts/toggle_compliances.js"></script>
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <script>
+            
+            function onLogin(token) {
+                document.getElementById("login").submit();
+            }
         </script>
     </head>
     <body>
