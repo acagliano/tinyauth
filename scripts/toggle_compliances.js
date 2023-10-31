b4e1635c47f15f63a47280392cb4cd41f662293f
Anthony Cagliano <acagliano97@gmail.com>
2023-10-31 00:29:35 -0400

    fix site key

---
+++ scripts/toggle_compliances.js
@@ -0,0 +1,8 @@
+
+function show_compliances() {
+    document.getElementById("compliances").style.display = "block";
+}
+
+function close_compliances() {
+    document.getElementById("compliances").style.display = "none";
+}
