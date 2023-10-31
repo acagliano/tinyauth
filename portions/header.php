a54f90e67ae0f07d657a1bdd4bc95bae3983377e
Anthony Cagliano <acagliano97@gmail.com>
2023-10-31 14:10:02 -0400

    fix form

--- portions/header.php
+++ portions/header.php
@@ -4,6 +4,7 @@
         <div id="infoline">Key-Based Authentication<br />for the TI-84+ CE</div>
         <?php
         if(isset($_SESSION["email"])){
+                echo "<br />";
                 echo "<form id=\"logout\" action=\"".filter_input(INPUT_SERVER, "PHP_SELF", FILTER_SANITIZE_URL)."\" method=\"post\">";
                 echo "<input type=\"submit\" name=\"logout\" value=\"Log Out\" />";
                 if (isset($_SESSION["time"])){
