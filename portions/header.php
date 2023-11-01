47e4d2a82fc927f1232ac93a201905f7196dd6bf
Anthony Cagliano <acagliano97@gmail.com>
2023-11-01 16:55:50 -0400

    fix form

--- portions/header.php
+++ portions/header.php
@@ -29,7 +29,8 @@
                         echo "</form>";
                         echo "<form id=\"danger\">";
                         echo "<input type=\"submit\" name=\"renew-keygen-secret\" value=\"Reset Keygen Secret\" /><br />";
-                        echo "<input type=\"submit\" name=\"renew-2fa-secret\" value=\"Reset 2FA Secret\" />";
+                        echo "<input type=\"submit\" name=\"renew-2fa-secret\" value=\"Reset 2FA Secret\" /><br />";
+                        echo "<input type=\"submit\" name=\"delete-account\" value=\"Delete Account\" />";
                         echo "</form>";
                         if(isset($_SESSION["otp-qr"])){
                                 $otp = TOTP::createFromSecret($_SESSION["secret_2fa"]);
