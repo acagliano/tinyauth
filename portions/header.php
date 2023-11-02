b438bfb9d9a543a3b057bad12d8f8c992bda4aec
Anthony Cagliano <acagliano97@gmail.com>
2023-11-01 20:00:20 -0400

    fix form

--- portions/header.php
+++ portions/header.php
@@ -19,6 +19,7 @@
                 }
                 else {
                         echo "<div style=\"width:80%; margin:auto; text-align:left;\">";
+                        echo "<hr />";
                         echo "<form id=\"logout\" action=\"".filter_input(INPUT_SERVER, "PHP_SELF", FILTER_SANITIZE_URL)."\" method=\"post\">";
                         echo "<input type=\"email\" value=\"".$_SESSION["email"]."\" readonly />";
                         echo "<input type=\"submit\" name=\"logout\" value=\"Logout\" />";
@@ -38,6 +39,7 @@
                         echo "<input type=\"submit\" name=\"renew-2fa-secret\" value=\"Reset 2FA Secret\" /><br />";
                         echo "<input type=\"submit\" name=\"delete-account\" value=\"Delete Account\" />";
                         echo "</form>";
+                        echo "<hr />";
                         if(isset($_SESSION["otp-qr"])){
                                 $otp = TOTP::createFromSecret($_SESSION["secret_2fa"]);
                                 echo "<hr />";
