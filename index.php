df88b0a3a6248fa86358a13fd52b7798548bfa75
Anthony Cagliano <acagliano97@gmail.com>
2023-11-01 20:10:02 -0400

    fix form

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ if(isset($_POST["email-otp"])){
         <?php include_once($_SERVER["DOCUMENT_ROOT"]."/portions/header.php"); ?>
         <div id="content">
             <br />
-            <p>TInyAuth is an application providing a secure means for public-facing services for the TI-84+ CE to authorize users without requiring that users authenticate repeatedly. Users authorize their calculators to access their TInyAuth account by downloading a keyfile from their Dashboard.</p><br />
+            <p>TInyAuth is an application providing a secure means for public-facing services for the TI-84+ CE to authorize users without requiring that users authenticate repeatedly or input credentials. Users authorize their calculators to access their TInyAuth account by downloading a keyfile from their Dashboard which contains a signed access token for their account.</p><br />
             <h3>Secure Keyfiles</h3>
             <p>Credentialing keyfiles are digitally-signed by TInyAuth to prevent forgery. Keys are valid for 1 year. Users may issue multiple keys against their account secret if needed.</p><br />
             <h3>Oauth2 Backend</h3>
