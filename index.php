4135f38d596fdca99ab28e075520372c9e6716c0
Anthony Cagliano <acagliano97@gmail.com>
2023-11-09 13:22:13 -0500

    fix unable to open file

--- index.php
+++ index.php
@@ -20,12 +20,59 @@ $env = parse_ini_file($_SERVER["DOCUMENT_ROOT"]."/.env");
 
 
 include_once $_SERVER["DOCUMENT_ROOT"]."/scripts/send-email.php";
-include_once($_SERVER["DOCUMENT_ROOT"]."/scripts/generate-keyfile.php");
-
 
 if(isset($_POST["logout"])){
     unset($_SESSION);
     session_destroy();
+}
+
+if(isset($_POST["generate-keyfile"])){
+    $kf_errors = array();
+    $privkey_file = $_SERVER["DOCUMENT_ROOT"]."/.secrets/privkey.pem";
+    $privkey = openssl_get_privatekey(file_get_contents($privkey_file), $env["SSL_PASS"]);
+    if($privkey){
+        openssl_sign($_SESSION["id"].$_SESSION["api_secret"], $signature, $privkey, openssl_get_md_methods()[14]);
+        $asn1_userid = new Integer($_SESSION["id"]);
+        $asn1_usertoken = new OctetString(bin2hex($signature));
+        $asn1_keydata = new Sequence($asn1_userid, $asn1_usertoken);
+        $asn1_raw = $asn1_keydata->getBinary();
+        $tfile = "/tmp/". uniqid(rand(), true);
+        $binfile = $tfile.".bin";
+        $appvfile = $tfile.".8xv";
+        $fname = "TInyKF";
+        $f = fopen($binfile, 'wb+');
+        if($f){
+            fwrite($f, "\x54\x49\x41\x55\x54\x48".$asn1_raw);
+            fclose($f);
+        } else {
+            $kf_errors[] = "Binary IO error.";
+            exit();
+        }
+        $output = shell_exec("../convbin/bin/convbin -i $binfile -j bin -o $appvfile -k 8xv -n $fname 2>&1");
+        error_log($output);
+        if(file_exists($appvfile)){
+            ob_clean();
+            header_remove();
+            header('Content-Description: File Transfer');
+            header('Content-Type: application/octet-stream');
+            header('Content-Disposition: attachment; filename="'.$fname.'.8xv"');
+            header('Content-Transfer-Encoding: binary');
+            header('Expires: 0');
+            header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+            header('Pragma: public');
+            header('Content-Length: ' . filesize($appvfile));
+            ob_clean();
+            flush();
+            echo file_get_contents($appvfile);
+            unlink($binfile);
+            unlink($appvfile);
+            exit();
+        } else {
+            $kf_errors[] = "Error creating download.";
+        }
+    } else {
+        $kf_errors[] = "OpenSSL: Error opening signing key.";
+    }
 }
 
 if(isset($_POST["login"])){
